387018eb1d94e3c23c93986de58f2e137a696e79
Zachary <60475836+zacharyt-cs@users.noreply.github.com>
2023-06-08 15:03:09 +0800

    exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Data/portfolio.csv', 'rt') as f:
+    next(f)
+    total_cost = 0
+    for line in f:
+        row = line.split(',')
+        total_cost += int(row[1]) * float(row[2])
+
+    print(f'Total cost {total_cost}')
